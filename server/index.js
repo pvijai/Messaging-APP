a0f7633f8d3b13b86fcc9b1ef7a071bd3bf3c418
pvijai <priyankavijai@yahoo.in>
2023-10-03 12:20:37 -0700

    update

--- server/index.js
+++ server/index.js
@@ -2,3 +2,12 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const cors= require("cors");
+
+app.use(cors());
+
+const server= http.createServer(app);
+
+
+server.listen(3001 ,() =>{
+     console.log("SERVER HAS STARTED RUNNING")
+});
